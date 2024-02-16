58e477132fdf0315a15ba05a2cc3804323d01dad
Prakash Adhikari <115913584+prakash7696@users.noreply.github.com>
2024-02-16 18:06:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ import io
 
 app = Flask(__name__)
 
-# Load the ViT model and feature extractor
+
 feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
 model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Check if the request contains a file
+    
     if 'file' not in request.files:
         return jsonify({'error': 'No file provided'})
 
-    # Read the image file from the request
+    
     file = request.files['file']
     
-    # Read image data
+   
     image_bytes = file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
 
-    # Process the image with ViT
+    
     inputs = feature_extractor(images=image, return_tensors="pt")
     outputs = model(**inputs)
     logits = outputs.logits
 
-    # Get the predicted class index
+    
     predicted_class_idx = logits.argmax(-1).item()
     predicted_class = model.config.id2label[predicted_class_idx]
 
